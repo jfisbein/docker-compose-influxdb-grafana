7961bbc8e38465be13f26ad32f770fda7f446775
Joan Fisbein <joan@fisbein.com>
2020-03-15 10:50:39 +0100

    Added slack and telegram integrations

---
+++ reset.sh
@@ -0,0 +1,5 @@
+sudo rm -rf /opt/grafana/storage/*
+sudo rm -rf /opt/chronograf/storage/*
+sudo rm -rf /opt/influxdb/storage/*
+sudo rm -rf /opt/influxdb/backups/*
+sudo rm -rf /opt/kapacitor/storage/*
