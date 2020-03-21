235d195f5e9fb63b7a81018395f0d786f289a9e3
Joan Fisbein <joan@fisbein.com>
2020-03-21 10:37:01 +0100

    Addded usage and removed verbosity

--- createPassword.sh
+++ createPassword.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+EXPECTED_ARGS=2
+if [ $# -ne ${EXPECTED_ARGS} ]; then
+  echo "Script to create credentials for Traefik basicauth"
+  echo ""
+  echo "Usage: $(basename "${0}") {username} {password}"
+  exit 1
+fi
+
 USERNAME="${1}"
 PASSWORD="${2}"
+docker pull xmartlabs/htpasswd > /dev/null
 docker run --rm -ti xmartlabs/htpasswd "${USERNAME}" "${PASSWORD}" | sed -e s/\\$/\\$\\$/g
