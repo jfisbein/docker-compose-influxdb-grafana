cb7664d8d6308284ccd4be33e8bfbe2e6988f352
Joan Fisbein <joan@fisbein.com>
2020-03-21 10:37:16 +0100

    Improved

--- reset.sh
+++ reset.sh
@@ -1,5 +1,6 @@
 sudo rm -rf /opt/grafana/storage/*
 sudo rm -rf /opt/chronograf/storage/*
 sudo rm -rf /opt/influxdb/storage/*
-sudo rm -rf /opt/influxdb/backups/*
+# sudo rm -rf /opt/influxdb/backups/*
 sudo rm -rf /opt/kapacitor/storage/*
+# docker system prune -af
