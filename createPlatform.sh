1f7241871a8e4495dbc2455f6e3de91de70c00a6
Joan Fisbein <joan@fisbein.com>
2020-04-26 11:56:10 +0200

    Added mail server folders

--- createPlatform.sh
+++ createPlatform.sh
@@ -5,4 +5,5 @@ mkdir -p /opt/chronograf/storage
 mkdir -p /opt/influxdb/storage
 mkdir -p /opt/influxdb/backups
 mkdir -p /opt/kapacitor/storage
+mkdir -p /opt/smtp-gmail-api/tokens
 mkdir -p letsencrypt
